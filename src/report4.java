056cd8efcece59fccd1a4e54141594734fbe7bf6
Shimoji Reo <e195758@ie.u-ryukyu.ac.jp>
2019-12-09 22:28:59 +0900

    report4-3

--- src/report4.java
+++ src/report4.java
@@ -1,16 +1,20 @@
 public class report4 {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
+        //try{
+            //String str = null;
+            //System.out.println(str.length());
 
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生した");
-            System.out.println(e.getMessage());
-        }
-
+        //}catch(NullPointerException e){
+            //System.out.println("NullPointerExceptionが発生した");
+            //System.out.println(e.getMessage());
+        //}
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
 
     }
+
+
+
 
 
 
